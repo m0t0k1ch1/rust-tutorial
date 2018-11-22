037b28087553f6603cbf7cb1acd87c5c7eee9bcd
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-11-22 23:06:09 +0900

    [guessing_game] align

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,6 +13,7 @@ fn main() {
         println!("Please input your guess");
 
         let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
