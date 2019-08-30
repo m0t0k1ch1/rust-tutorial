c88ce191a8ed3d228aef5bb2cb2122c91e54e155
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-08-30 18:50:28 +0900

    [control_flow_match] update

--- control_flow_match/src/main.rs
+++ control_flow_match/src/main.rs
@@ -25,6 +25,15 @@ fn value_in_cents(coin: Coin) -> u8
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32>
+{
+    match x
+    {
+        None    => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main()
 {
     println!("1 penny is {} cents.",   value_in_cents(Coin::Penny));
@@ -32,4 +41,30 @@ fn main()
     println!("1 dime is {} cents.",    value_in_cents(Coin::Dime));
     println!("1 quarter is {} cents.", value_in_cents(Coin::Quarter(UsState::Alabama)));
     println!("1 quarter is {} cents.", value_in_cents(Coin::Quarter(UsState::Alaska)));
+
+    let five = Some(5);
+    let six  = plus_one(five);
+    let none = plus_one(None);
+
+    match six
+    {
+        Some(i) => println!("six is {}", i),
+        None    => println!("six is none"),
+    };
+    match none
+    {
+        Some(i) => println!("none is {}", i),
+        None    => println!("none is None"),
+    };
+
+    let some_u8_value = 0u8;
+
+    match some_u8_value
+    {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    };
 }
